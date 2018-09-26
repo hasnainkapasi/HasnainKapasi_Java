5cd4c56c6167bec71438d362d2b875daab906525
Hasnain Kapasi <hasnain.kapasi@gmail.com>
2018-09-27 03:00:09 +0530

    Added comments and removed tab characters

--- general-ledger-system/src/main/java/com/abc/general/ledger/system/GeneralLedgerSystemApplication.java
+++ general-ledger-system/src/main/java/com/abc/general/ledger/system/GeneralLedgerSystemApplication.java
@@ -16,16 +16,21 @@ import com.abc.general.ledger.system.file.writer.EodPositionFileWriter;
 import com.abc.general.ledger.system.file.writer.impl.EodPositionCsvFileWriterImpl;
 import com.abc.general.ledger.system.processor.TransactionProcessor;
 
+/**
+ * General Ledger System main class to process stock transactions and generate EOD positions of accounts. 
+ * @author Hasnain Kapasi
+ */
 public class GeneralLedgerSystemApplication 
 {
     public static void main(String[] args)
     {
-    	if (args.length != 3) {
-    		System.out.println("Invalid number of arguments are passed!!!");
-    		System.out.println("Execute jar as below:");
-    		System.out.println("java -jar general-ledger-system.jar <position file path> <transaction file path> <output file folder>");
-    		return;
-    	}
+        if (args.length != 3) {
+            System.out.println("Invalid number of arguments are passed!!!");
+            System.out.println("Execute jar as below:");
+            System.out.println("java -jar general-ledger-system.jar "
+                    + "<position file path> <transaction file path> <output file folder>");
+            return;
+        }
         String outputFileDirPath = args[2];
         File outputFileDirectory = new File(String.valueOf(outputFileDirPath));
         if (!outputFileDirectory.exists()) {
@@ -46,10 +51,10 @@ public class GeneralLedgerSystemApplication
                 .sorted(Comparator.comparing(eodp -> Math.abs(((EodPosition)eodp).getDelta())))
                 .collect(Collectors.toList());
         System.out.println(
-                String.format("instrument with lowest net transaction volume for the day is %s with delta equal to %d",
+              String.format("instrument with lowest net transaction volume for the day is %s with delta equal to %d",
                         eodPositions.get(0).getStockSymbol(), eodPositions.get(0).getDelta()));
         System.out.println(
-                String.format("instrument with largest net transaction volume for the day is %s with delta equal to %d",
+              String.format("instrument with largest net transaction volume for the day is %s with delta equal to %d",
                         eodPositions.get(eodPositions.size() - 1).getStockSymbol(),
                         eodPositions.get(eodPositions.size() - 1).getDelta()));
     }
